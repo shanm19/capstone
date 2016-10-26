98c47f36b62ef03607c68803bd68ae18e06f75b0
susansico <susan.sico@gmail.com>
2016-10-25 21:53:39 -0700

    Installed node modules which created package.json and added code to server.js

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,43 @@
 /* capstone server.js */
+
+// Require packages
+var express = require("express");
+var logger = require("morgan");
+var bodyParser = require("body-parser");
+//var expressJwt = require("express-jwt");
+var mongoose = require("mongoose");
+
+// Config environment variables
+var config = require("./config");
+var port = process.env.PORT || 8080;
+
+// Require routes
+
+// Create Server
+var app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(logger("dev"));
+
+// Connect to Mongoose
+//var database = path.join(config.db_host.config.db_name);
+/*mongoose.connect("mongodb://" + config.db_user + ":" + config.db_pass + "@" + database, function(err) {
+    if (err) console.log("Error connecting to MongoDB: ", err.message)
+    else console.log("Connected to MongoDB " + database);
+});*/
+
+// Serve up frontend files
+//app.use(express.static(path.join(__dirname, '..', '/frontend')));
+//app.use(express.static(path.join(__dirname, "..", "/frontend")));
+
+// Routes requiring authentication
+//app.use("/api", expressJwt({secret:config.secret}));
+
+// Routes without authentication
+//app.use("/auth", require("./routes/authRoutes");
+
+// Routes without authentication
+
+app.listen(port, function() {console.log("Server is listening on port", port)});
+
+
