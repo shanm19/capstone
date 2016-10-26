c1e5ef46401ecfa1eb7fb783e1b26a065abe5c89
susansico <susan.sico@gmail.com>
2016-10-26 09:09:32 -0700

    add config template

---
+++ backend/config-template.js
@@ -0,0 +1,13 @@
+/* capstone config.js */
+
+// Contains api keys and passwords and is not pushed to Github.
+
+process.env.PORT =  8080;
+
+module.exports = {
+    db_host: "",
+    db_name: "",
+    db_user: "",
+    db_pass: "",
+    db_secret: ""
+};
