18ff3344fae415aa1c985a7f8c31a968ae7ad7bf
susansico <susan.sico@gmail.com>
2016-11-03 16:48:33 -0700

    Finished subredditRouteProtected and commentRouteProtected.

--- backend/server.js
+++ backend/server.js
@@ -29,21 +29,21 @@ mongoose.connect("mongodb://" + config.db_user + ":" + config.db_pass + "@" + da
 app.use(express.static(path.join(__dirname, "..", "/frontend")));
 
 // Routes requiring authentication
-//app.use("/api", expressJwt({secret:config.db_secret}));
+app.use("/api", expressJwt({secret:config.db_secret}));
 require('./passport/passport')(app);
-// app.use("/api/user", require("./routes/userRouteProtected"));
-//app.use("/api/admin", require("./routes/adminRoute"));
-//app.use("/api/post", require("./routes/postRouteProtected"));
-//app.use("/api/subreddit", require("./routes/subredditRouteProtected"));
+app.use("/api/user", require("./routes/userRouteProtected"));
+app.use("/api/admin", require("./routes/adminRoute"));
+app.use("/api/post", require("./routes/postRouteProtected"));
+app.use("/api/subreddit", require("./routes/subredditRouteProtected"));
 app.use("/api/comment", require("./routes/commentRouteProtected"));
 
 // Routes without authentication
 app.use("/post", require("./routes/postRoute"));
-app.use("/subreddit", require("./routes/subredditRoute"));
+app.use("/subreddit", require("./../../../../subredditRoute"));
 app.use("/comment", require("./routes/commentRoute"));
 // require('./routes/authRoute')(app, passport);
 app.use("/auth", require("./routes/authRoute"))
-//app.use("/subreddit", require("./routes/subredditRoute"));
+app.use("/subreddit", require("./../../../../subredditRoute"));
 
 
 app.listen(port, function() {console.log("Server is listening on port", port)});
