0c44d17c3e9bf6cf01ca0cdd647cdc54a9411dea
Evan Morrison <evanrmorrison@gmail.com>
2016-11-04 02:25:43 -0600

    add view for single post with comments

--- frontend/js/controllers/postController.js
+++ frontend/js/controllers/postController.js
@@ -4,5 +4,11 @@ var app = angular.module('MockReddit');
 
 app.controller('PostController', ['$scope', '$routeParams', 'PostService', function($scope, $routeParams, PostService) {
 
+    (function getPostComments () {
+        PostService.getPostComments($routeParams.id)
+        .then(function(response){
+            $scope.post = response;
+        })
+    })();
 
 }]);
